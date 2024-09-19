208662b6b577e6b7012c1a6257a30f2324db7b32
Abdelquoddouss <fadliabdelquoddouss.06@gmail.com>
2024-09-19 16:14:15 +0100

    ajoute connection

---
+++ src/Repository/UserRepository.java
@@ -0,0 +1,12 @@
+package Repository;
+
+import java.sql.Connection;
+
+public class UserRepository {
+
+    private  Connection connection;
+
+
+
+
+}
--- src/config/DatabaseConnection.java
+++ src/config/DatabaseConnection.java
@@ -1,5 +1,30 @@
 package config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
+    private static Connection connection = null;
+
+    public DatabaseConnection() {
+    }
+
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            synchronized (DatabaseConnection.class) {
+                if (connection == null) {
+                    try {
+                        connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/BatiCuisine", "BatiCuisine", "");
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return connection;
+    }
+
 
 }
